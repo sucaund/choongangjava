098dcebb49d4cba9e13bb532713a9862b66b8633
sucaund <hsh7143@gmail.com>
2023-11-01 17:37:24 +0900

    20231101

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava {
+	
+	public static void main(String[] args) {
+
+		
+		System.out.println("HelloJava!");
+		System.out.println("안녕하세요!");
+		System.out.println("안녕하세요!");
+		
+	}
+}//class
